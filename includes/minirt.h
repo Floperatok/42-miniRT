f95df88f9626423c79d4b52baec76f655bfebb70
Floperatok <floperatox.spam@gmail.com>
2024-02-15 12:53:06 +0100

    Init workspace

---
+++ includes/minirt.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minirt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsalles <nsalles@student.42perpignan.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/15 12:47:05 by nsalles           #+#    #+#             */
+/*   Updated: 2024/02/15 12:49:33 by nsalles          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+# define MINIRT_H
+
+# include "../libs/libft/includes/libft.h"
+# include "../libs/minilibx-linux/mlx.h"
+
+#endif
