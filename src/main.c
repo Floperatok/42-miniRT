e914b6d28e80a8324864df09d1a028dab102766c
Floperatok <floperatox.spam@gmail.com>
2024-02-17 19:13:46 +0100

    renamed data

--- src/main.c
+++ src/main.c
@@ -6,60 +6,60 @@
 /*   By: nsalles <nsalles@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 12:38:38 by nsalles           #+#    #+#             */
-/*   Updated: 2024/02/17 16:18:23 by nsalles          ###   ########.fr       */
+/*   Updated: 2024/02/17 19:13:29 by nsalles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-int	exit_handling(t_minirt *data)
+int	exit_handling(t_minirt *mem)
 {
-	if (!data)
+	if (!mem)
 		exit(EXIT_FAILURE);
-	if (data->win)
-		destroy_image(data->img, data->win->mlx);
-	destroy_window(data->win);
-	free(data);
+	if (mem->win)
+		destroy_image(mem->img, mem->win->mlx);
+	destroy_window(mem->win);
+	free(mem);
 	exit(EXIT_SUCCESS);
 }
 
-t_minirt	*create_data(void)
+t_minirt	*init_mem(void)
 {
-	t_minirt *data;
+	t_minirt *mem;
 
-	data = malloc(sizeof(t_minirt));
-	if (!data)
+	mem = malloc(sizeof(t_minirt));
+	if (!mem)
 	{
-		print_error("Fatal error: data initialization: Out of memory\n");
-		exit_handling(data);
+		print_error("Fatal error: mem initialization: Out of memory\n");
+		exit_handling(mem);
 	}
-	data->img = NULL;
-	data->win = NULL;
-	data->shapes = NULL;
-	return (data);
+	mem->img = NULL;
+	mem->win = NULL;
+	mem->shapes = NULL;
+	return (mem);
 }
 
 int main(int ac, char **av)
 {
-	t_minirt	*data;
+	t_minirt	*mem;
 
 	if (ac != 2)
 		return (print_error("Error: One argument needed.\n"), 1);
 	if (!check_file(av[1]))
 		return (1);
-	data = create_data();
-	data->win = get_window();
-	if (!data->win)
-		exit_handling(data);
-	data->img = get_image(data->win);
-	if (!data->img)
-		exit_handling(data);
+	mem = init_mem();
+	mem->win = get_window();
+	if (!mem->win)
+		exit_handling(mem);
+	mem->img = get_image(mem->win);
+	if (!mem->img)
+		exit_handling(mem);
 	// get_values()
-	mlx_hook(data->win->window, 17, 0L, &exit_handling, data);
-	mlx_hook(data->win->window, 2, 1L << 0, &user_input, data);
-	draw_image(NULL, data->img);
-	mlx_put_image_to_window(data->win->mlx, data->win->window,\
-		data->img->image, 0, 0);
-	mlx_loop(data->win->mlx);
+	mlx_hook(mem->win->window, 17, 0L, &exit_handling, mem);
+	mlx_hook(mem->win->window, 2, 1L << 0, &user_input, mem);
+	draw_image(NULL, mem->img);
+	mlx_put_image_to_window(mem->win->mlx, mem->win->window,\
+		mem->img->image, 0, 0);
+	mlx_loop(mem->win->mlx);
 	return (0);
 }
