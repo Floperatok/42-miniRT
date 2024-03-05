b86d144c30ed039b47f992a84060390a5f66cd22
Floperatok <floperatox.spam@gmail.com>
2024-03-05 17:07:20 +0100

    fix shadows not correctly working with objects the other side of the light

--- src/computations/light/light_source.c
+++ src/computations/light/light_source.c
@@ -6,7 +6,7 @@
 /*   By: nsalles <nsalles@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 16:00:42 by nsalles           #+#    #+#             */
-/*   Updated: 2024/03/05 16:22:31 by nsalles          ###   ########.fr       */
+/*   Updated: 2024/03/05 17:00:30 by nsalles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,19 @@
 int	apply_light(t_light *light, t_hitinfo *hit, t_objects *objs)
 {
 	t_point		light_dir;
+	double		light_dst;
 	t_color		color;
 	double		exposure;
 
 	if (!light)
 		return (hit->color);
 	light_dir = soustract_vect(light->pos, hit->hit_point);
+	light_dst = ft_lenght(light_dir);
 	normalize_vect(&light_dir);
 	exposure = dot(hit->normal_dir, light_dir);
 	if (exposure < 0)
 		return (hit->color);
-	if (is_in_shadow(hit->hit_point, light_dir, objs))
+	if (is_in_shadow(hit->hit_point, light_dir, light_dst, objs))
 		return (hit->color);
 	exposure *= 5;
 	exposure++;
--- src/computations/light/shadow.c
+++ src/computations/light/shadow.c
@@ -6,7 +6,7 @@
 /*   By: nsalles <nsalles@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 15:51:24 by nsalles           #+#    #+#             */
-/*   Updated: 2024/03/05 16:21:49 by nsalles          ###   ########.fr       */
+/*   Updated: 2024/03/05 16:59:58 by nsalles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 /*
  *	Returns whether or not there is objects between the start_pos and the light.
 */
-int	is_in_shadow(t_point start_pos, t_point light_dir, t_objects *objs)
+int	is_in_shadow(t_point start_pos, t_point light_dir, double light_dst,
+	t_objects *objs)
 {
 	t_ray		ray;
 	t_hitinfo	hit;
@@ -23,5 +24,5 @@ int	is_in_shadow(t_point start_pos, t_point light_dir, t_objects *objs)
 	ray.dir = light_dir;
 	ray.origin = start_pos;
 	hit = ray_collision(ray, objs);
-	return (hit.did_hit);
+	return (hit.did_hit && hit.dst < light_dst);
 }
