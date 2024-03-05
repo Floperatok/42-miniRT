0e6ae1257a9b731e465ce39c6bc96d651ab2e5d6
Floperatok <floperatox.spam@gmail.com>
2024-03-05 14:29:33 +0100

    fix plane normal vector

--- src/computations/collisions/planes.c
+++ src/computations/collisions/planes.c
@@ -6,7 +6,7 @@
 /*   By: nsalles <nsalles@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 14:37:05 by nsalles           #+#    #+#             */
-/*   Updated: 2024/03/04 15:40:06 by nsalles          ###   ########.fr       */
+/*   Updated: 2024/03/05 14:25:46 by nsalles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ void	planes_collision(t_ray ray, t_plane **planes, t_hitinfo *closest_hit)
 	closest_hit->did_hit = 1;
 	closest_hit->hit_point = add_vect(ray.origin,
 		multiply_vect(ray.dir, closest_hit->dst));
-	closest_hit->normal_dir = soustract_vect(closest_hit->hit_point, closest_plane->pos);
+	closest_hit->normal_dir = closest_plane->normal;
 	normalize_vect(&closest_hit->normal_dir);
 	closest_hit->color = closest_plane->color;
 }
