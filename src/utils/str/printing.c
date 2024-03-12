65a72927be49a09c10fddd0b0a883672a39c44b6
Floperatok <floperatox.spam@gmail.com>
2024-03-12 18:20:04 +0100

    fix user input error message

--- src/utils/str/printing.c
+++ src/utils/str/printing.c
@@ -6,7 +6,7 @@
 /*   By: nsalles <nsalles@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 17:46:55 by drenassi          #+#    #+#             */
-/*   Updated: 2024/03/12 17:54:58 by nsalles          ###   ########.fr       */
+/*   Updated: 2024/03/12 18:19:30 by nsalles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 void	print_usage(void)
 {
-	printf("Usage :\n ./miniRT your_map.rt \
-		[HardwareMode]\nHardwareMode:0=SingleThread ; \
-		1=MultipleThreads ; 2=GPU\n");
+	printf("Usage :\n./miniRT your_map.rt "
+	"[HardwareMode]\nHardwareMode :\n- 0 = SingleThread\n" 
+	"- 1 = MultipleThreads\n- 2 = GPU\nDefault : SingleThread\n");
 }
 
 int	print_error(char *msg)
